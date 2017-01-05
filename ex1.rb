d13f9c7bfd71ba390c3de17c50095dc22a2bffce
Olivia Boertje <malevolentninja@gmail.com>
2017-01-05 12:55:52 +0000

    Create ex1.rb
    The First excercise.

---
+++ ex1.rb
@@ -0,0 +1,11 @@
+puts "Hello World!" #prints out the string
+puts "Hello Again" #prints another string
+puts "I like typing this." #prints another string
+puts "This is fun." #prints another string
+
+puts "I'd much rather you 'not'." #prints another string which uses both the " " and ' '
+puts "'I' said do not touch this.'" # prints another string with the same style as the last line
+puts "Here is another line."
+
+# -*- coding: utf-8 -*- sometimes needed to be added
+# the octothorpe/hash char turns the line into a comment
