23741e46c6cd709f6675ba8959fe8e1610257547
Olivia Boertje <malevolentninja@gmail.com>
2017-01-05 16:13:44 +0000

    Update and rename ex3.1Floating_Numbers to ex3.1Floating_Numbers.rb

---
+++ ex3.1Floating_Numbers.rb
@@ -0,0 +1,26 @@
+#Rewrite ex3.rb to use floating point numbers so it's more accurate. 20.0 is floating point.
+
+# floating point is decimal, also refered to as 'true numbers'.
+
+puts "I will now count my chickens:" # A string printed regaring chickens to be counted
+
+puts "Hens #{27.4 + 40 / 3.3 }" # 39.521212121212116
+puts "Roosters #{100 - 23.6 * 3 % 4}" #97.19999999999999
+
+puts "Now I will count the eggs:" # 10.086229314420804
+
+puts 3.43 + 2.25 + 1.167 - 5.3 + 4.678 % 2.8 - 1.01 / 4.23 + 6.9
+
+puts "Is it true that 3.9 + 2.9 < 5.9 - 7.9?"
+
+puts 3.9 + 2.9 < 5.9 -7.9
+
+puts "What is 5.9 - 7.9? #{5.9 - 7.9}" #false
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5.89 > - 2.80}" #true
+puts "Is it greater or equal? #{5.89 >= -2.80}" # true
+puts "Is it less or equal? #{5.89 <= -2.80}" # false
