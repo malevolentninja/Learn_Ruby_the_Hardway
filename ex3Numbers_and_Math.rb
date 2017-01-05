423f62404fd508355b5cf790223d63c19e83e8a9
Olivia Boertje <malevolentninja@gmail.com>
2017-01-05 16:05:46 +0000

    Create ex3Numbers_and_Math.rb

---
+++ ex3Numbers_and_Math.rb
@@ -0,0 +1,34 @@
+
+# + plus
+# - minus
+# / slash
+# * asterisk
+# % percent
+# < less-than
+# > greather-than
+# <= less-than-equal
+# >= greater-than-equal
+
+puts "I will now count my chickens:" # A string printed regaring chickens to be counted
+
+puts "Hens #{25 + 30 / 6}" # This is how may hens are available
+puts "Roosters #{100 - 25 * 3 % 4}" #How many roosters are available
+
+puts "Now I will count the eggs:" # string regarding the counting of eggs
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # integers totalling the amount of eggs
+
+puts "Is it true that 3 + 2 < 5 - 7?" # A string querying integers
+
+puts 3 + 2 < 5 -7 # A set of numbers to work out the answer to the string question earlier
+
+puts "What is 3 + 2? #{3 + 2}" # string and integer combination
+puts "What is 5 - 7? #{5 - 7}" # string and integer combination
+
+puts "Oh, that's why it's false." # A simply string
+
+puts "How about some more." # Another simple string
+
+puts "Is it greater? #{5 > - 2}" #askign if the integers are greater
+puts "Is it greater or equal? #{5 >= -2}" # asking if the integers are equal
+puts "Is it less or equal? #{5 <= -2}" # asking if the integers are less or equal
