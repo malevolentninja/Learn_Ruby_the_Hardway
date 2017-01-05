e76e457257c07ee4ac8b3eab606d352c01728f81
Olivia Boertje <malevolentninja@gmail.com>
2017-01-05 17:48:13 +0000

    Create ex7Printing.rb

---
+++ ex7Printing.rb
@@ -0,0 +1,24 @@
+puts "Mary had a little lamb." # prints a string
+puts "Its fleece was white as #{'snow'}." # prints a string with an array
+puts "And everywhere that Mary went." # prints a string
+puts "." * 10 # what'd that do?  - prints dots ten times
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+# These are a list of variables which have one string for each
+# what that print vs puts on this line what's it do?
+print end1 + end2 + end3 + end4 + end5 + end6 #using print not matter how many commands it will keep the print results on the same line
+puts end7 + end8 + end9 + end10 + end11 + end12 #changing to puts displays on one line
+puts " " #forces a line break alternative to \n
+
+# print and puts seem to be relatively same, the result is CheeseBurger spelt.
